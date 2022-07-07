2983eab5144554fd1f511de1d7c6ee66da07a1d8
Manon-POTEL <manonpotel@hotmail.fr>
2022-07-07 12:18:06 +0200

    Ajout des premiers models

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class test extends Model
+class Roles extends Model
 {
     use HasFactory;
+
+    protected $table = 'roles';
+    protected $fillable = ['name'];
 }
