72c88d22e854346eeac200f6582c95b56b5aacbb
AezardSR <69091066+AezardSR@users.noreply.github.com>
2022-09-13 11:02:51 +0200

    début authController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,5 +8,33 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    //inscription
+    public function InscrisUtilisateur(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'password_confirmation' => 'required',
+        ]);
+
+        $utilisateur = new User;
+        
+        $utilisateur->first_name = $request->first_name;
+        $utilisateur->last_name = $request->last_name;
+        $utilisateur->email =   $request->email;
+        $utilisateur->password  = bcrypt($request->password);
     
+        $utilisateur->save();
+
+        return response()->json([
+            'msg'=>'Utilisateur creation reussi',
+            'status_code' => 200,
+            'utilisateur'=> $utilisateur
+            ]);  
+    }
+
+    
+
 }
+
+
