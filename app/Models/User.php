18e428e9a6411957e41fafb975a7cd294570d7fb
AezardSR <69091066+AezardSR@users.noreply.github.com>
2022-12-05 12:13:39 +0100

    il y a de la magie dans l'air

---
+++ app/Models/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *@var array
+     *
+     */
+    protected $table = 'users';
+    protected $fillable = ['name','firstname','birthdate','mail','tel','password','adress','city','zipCode','roles_id', 'types_id'];
+
+    public function roleUser(){
+        return $this->hasOne(Roles::class);
+    }    
+    public function typeUser(){
+        return $this->hasOne(Types::class);
+    }
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+ 
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
---
+++ database/factories/UserFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Roles;
+use App\Models\Types;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'firstname' => $this->faker->firstName(),
+            'birthdate' => $this->faker->date(),
+            'mail' => $this->faker->unique()->safeEmail(),
+            'tel' => $this->faker->randomNumber(9, true),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'address' => $this->faker->address(),
+            'city' => $this->faker->city(),
+            'zipCode' => $this->faker->randomNumber(5, true),
+            'roles_id' => Roles::all()->random()->id,
+            'types_id' => Types::all()->random()->id,
+        ];
+
+    }
+}
