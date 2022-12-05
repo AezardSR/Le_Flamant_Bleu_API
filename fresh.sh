1f815675cee6cada3fdc0abf5af704e458d01f84
AezardSR <69091066+AezardSR@users.noreply.github.com>
2022-12-05 11:33:44 +0100

    rep docker

--- fresh.sh
+++ fresh.sh
@@ -2,7 +2,7 @@
 
 # Stop and delete all containers
 # FR: Arrete et supprime tous les containers
-sudo docker compose down api
+sudo docker compose down 
 
 # Install php dependencies
 # FR: Installer les dépendances php
@@ -10,14 +10,14 @@ sudo docker compose run api composer install
 
 # Fresh migrate + seeding
 # FR: Migrer et remplir la base de données
-sudo docker compose run api php artisan migrate:fresh --seed
+sudo docker compose run api php artisan migrate:fresh --seed 
 
 # Generate key
 # FR: Generation de la clé
-sudo docker compose run api php artisan key:generate
+sudo docker compose run api php artisan key:generate 
 
 # Launch containers
 # FR: Lancer les containers
-sudo docker compose up -d
+sudo docker compose up -d 
 
 
