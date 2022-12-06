0193c707faee0acb6a27691c6817225364dcf2af
Manon-POTEL <manonpotel@hotmail.fr>
2022-12-06 10:05:27 +0100

    Pas touche à mes routes !!!

--- routes/api.php
+++ routes/api.php
@@ -67,7 +67,54 @@ Route::post('/types', [RolesController::class, 'addTypes']);
 
 //Routes pour Modules
 Route::get('/modules', [LessonController::class, 'getModuleList']);
+Route::post('/modules', [LessonController::class, 'addModule']);
+Route::delete('/modules/{id}/{moduleName}', [LessonController::class, 'deleteModule']);
+Route::put('/modules', [LessonController::class, 'changeModule']);
 
+//Routes pour Parties
+Route::get('/part', [LessonController::class, 'getPartsList']);
+Route::post('/part', [LessonController::class, 'addParts']);
+Route::delete('/part/{id}/{partName}', [LessonController::class, 'deleteParts']);
+Route::put('/part', [LessonController::class, 'changeParts']);
+
+//RegistrationController
+Route::get('/registrationTypes', [RegistrationController::class, 'getRegistrationTypeList']);
+Route::get('/registrations', [RegistrationController::class, 'getRegistrationsList']);
+Route::get('/registration/{id}', [RegistrationController::class, 'getOneRegistration']);
+Route::post('/registration/create/{dateRegistration}/{detailRegistration}/{id_promos}/{id_registrationTypes}', [RegistrationController::class, 'createRegistration']);
+Route::put('/registration/update/{id}/{column}/{newValue}', [RegistrationController::class, 'editRegistration']);
+Route::put('/registrationType/update/{id}/{column}/{newValue}', [RegistrationController::class, 'editRegistrationType']);
+Route::get('/signatures/{id_registration}', [RegistrationController::class, 'getSignatureList']);
+Route::post('/signatures/{id_users}/{id_registrations}/{date}', [RegistrationController::class, 'addSignature']);
+//Routes pour Leçons
+Route::get('/leçons', [LessonController::class, 'getLessonList']);
+Route::post('/leçons', [LessonController::class, 'addLesson']);
+Route::delete('/leçons/{id}/{content}/{id_parts}', [LessonController::class, 'deleteLesson']);
+Route::put('/leçons', [LessonController::class, 'changeLesson']);
+
+//Routes pour Exercices
+Route::get('/exercice', [LessonController::class, 'getExerciceList']);
+Route::post('/exercice', [LessonController::class, 'addExercice']);
+Route::delete('/exercice/{id}/{name}/{content}/{id_parts}', [LessonController::class, 'deleteExercice']);
+Route::put('/exercice', [LessonController::class, 'changeExercice']);
+
+//Routes pour Catégories
+Route::get('/categories', [LessonController::class, 'getCategoriesList']);
+Route::post('/categories', [LessonController::class, 'addCategories']);
+Route::delete('/categories/{id}', [LessonController::class, 'deleteCategories']);
+Route::put('/categories', [LessonController::class, 'changeCategories']);
+
+//Routes pour Modules Catégories
+Route::get('/modulescategories', [LessonController::class, 'getModulesCategoriesList']);
+Route::post('/modulescategories', [LessonController::class, 'addModulesCategories']);
+Route::delete('/modulescategories/{id}/{id_categories}/{id_modules}', [LessonController::class, 'deleteModulesCategories']);
+Route::put('/modulescategories', [LessonController::class, 'changeModulesCategories']);
+
+//Routes pour Modules Classes
+Route::get('/modulesclasses', [LessonController::class, 'getModulesClassesList']);
+Route::post('/modulesclasses', [LessonController::class, 'addModulesClass']);
+Route::delete('/modulesclasses/{id}/{id_classes}/{id_modules}', [LessonController::class, 'deleteModulesClasses']);
+Route::put('/modulesclasses', [LessonController::class, 'changeModulesClass']);
 
 //Routes pour Documents
 Route::get('/documents', [DocumentsController::class, 'getDocuments']);
@@ -89,7 +136,25 @@ Route::put('/jobsoffers/update/{id}', [JobsOffersController::class, 'changeJobsO
 
 //Routes pour rendez-vous
     //Type de rdv
+    Route::get('/appointmentstypes', [CalendarController::class, 'getAppointmentsTypes']);
+    Route::post('/appointmentstypes', [CalendarController::class, 'addAppointmentsTypes']);
+    Route::delete('/appointmentstypes/{id}/{name}', [CalendarController::class, 'deleteAppointmentsTypes']);
+    Route::put('/appointmentstypes', [CalendarController::class, 'changeAppointmentsTypes']);
 
+    //RDV
+    Route::get('/appointments', [CalendarController::class, 'getAppointments']);
+    Route::post('/appointments', [CalendarController::class, 'addAppointments']);
+    Route::delete('/appointments/{id}/{titleDetails}/{descriptionDeatils}/{dateDetails}/{id_receiver}/{id_create}/{id_appointments_types}', [CalendarController::class, 'deleteAppointments']);
+    Route::put('/appointments', [CalendarController::class, 'changeAppointments']);
+//promosController
+Route::get('/Promos', [PromoController::class, 'getPromosList']);
+Route::post('/Promo/create/{name}/{startDate}/{endDate}/{duration}/{id_formationsTypes}/{id_formationsFormats}', [PromoController::class, 'createPromo']);
+Route::get('/Promo/{id}', [PromoController::class, 'getOnePromo']);
+Route::put('/Promo/{id}/{column}/{newValue}', [PromoController::class, 'editPromo']);
+
+Route::get('/PromoStudents', [PromoController::class, 'getPromoStudentsList']);
+Route::post('/PromoStudent/create/{id_students}/{id_promos}', [PromoController::class, 'AddStudentToPromo']);
+Route::get('/PromoStudent/{id_promos}', [PromoController::class, 'getPromoStudents']);
 Route::put('/PromoStudent/{id}/{column}/{newValue}', [PromoController::class, 'editPromoStudent']);
 
 Route::get('/PromoTeachers', [PromoController::class, 'getPromoTeachersList']);
@@ -99,6 +164,39 @@ Route::put('/PromoTeacher/{id}', [PromoController::class, 'editPromoTeacher']);
 
 Route::get('/PromoCalendars', [PromoController::class, 'getPromoCalendarList']);
 Route::post('/PromoCalendar/create/{startDate}/{endDate}/{id_promos}', [PromoController::class, 'AddPromoCalendar']);
+Route::get('/PromoCalendar/{id_promos}', [PromoController::class, 'getPromoCalendar']);
+Route::put('/PromoCalendar/{id}/{column}/{newValue}', [PromoController::class, 'editPromoCalendar']);
+
+//testsController
+Route::get('/ApplicantsTestSurvey', [TestsController::class, 'getApplicantsTestSurveyList']);
+Route::post('/ApplicantTestSurvey/create/{name}/{firstname}/{dateSurvey}/{mail}/{id_entranceTests}/{id_formationsFormats}/{id_promos}', [TestsController::class, 'createApplicantsTestSurvey']);
+Route::get('/ApplicantTestSurvey/{id}', [TestsController::class, 'getOneApplicantsTestSurvey']);
+Route::put('/ApplicantTestSurvey/{id}/{column}/{newValue}', [TestsController::class, 'editApplicantsTestSurvey']);
+
+Route::get('/ApplicantsAnswers', [TestsController::class, 'getApplicantsAnswersList']);
+Route::post('/ApplicantsAnswer/create/{answer}/{id_surveyAnswers}/{id_applicantsTestSurvey}', [TestsController::class, 'createApplicantsAnswers']);
+Route::get('/ApplicantsAnswer/{id}', [TestsController::class, 'getApplicantAnswers']);
+
+Route::get('/SurveyAnswers', [TestsController::class, 'getSurveyAnswersList']);
+Route::post('/SurveyAnswer/create/{answer}/{correctAnswer}/{id_surveys}', [TestsController::class, 'createSurveyAnswers']);
+Route::get('/SurveyAnswer/{id}', [TestsController::class, 'getOneSurveyAnswers']);
+Route::put('/SurveyAnswer/{id}/{column}/{newValue}', [TestsController::class, 'editSurveyAnswers']);
+
+Route::get('/Surveys', [TestsController::class, 'getSurveysList']);
+Route::post('/Survey/create/{survey}', [TestsController::class, 'createSurveys']);
+Route::get('/Survey/{id}', [TestsController::class, 'getOneSurvey']);
+Route::put('/Survey/{id}/{column}/{newValue}', [TestsController::class, 'editSurvey']);
+
+Route::get('/EntranceTestsSurvey', [TestsController::class, 'getEntranceTestsSurveyList']);
+Route::post('/EntranceTestsSurvey/create/{id_entranceTests}/{id_surveys}', [TestsController::class, 'createEntranceTestsSurvey']);
+Route::get('/EntranceTestsSurvey/{id}', [TestsController::class, 'getOneEntranceTestsSurvey']);
+Route::put('/EntranceTestsSurvey/{id}/{column}/{newValue}', [TestsController::class, 'editEntranceTestsSurvey']);
+
+//messagingController
+Route::post('/Message/create/{content}/{id_receiver}/{id_sender}', [MessagingController::class, 'addMessage']);
+Route::get('/Message/{id_receiver}/{id_sender}', [MessagingController::class, 'getChat']);
+Route::put('/Message/{id}/{column}/{newValue}', [MessagingController::class, 'editMessage']);
+Route::delete('/message/delete/{id}', [MessagingController::class, 'deleteMessage']);
 	
 // routes pour les questions
 Route::get('/questions/{id}', [QuestionsController::class, 'viewQuestion']);
