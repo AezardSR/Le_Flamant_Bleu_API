16bafc94cc9c12dba966d6c062a7f8b4a1f77d2d
BenjaminPmt <benjamin.pimont@laposte.net>
2022-12-09 16:31:13 +0100

    installation de swagger et premier GET fait

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -8,6 +8,40 @@ use Illuminate\Http\Request;
 
 class DocumentsController extends Controller
 {
+     /**
+     * @OA\Get(
+     *      path="/documents",
+     *      operationId="Documents",
+     *      tags={"documents"},
+
+     *      summary="Tous les documents",
+     *      description="Returns all countries and associated provinces. The country_slug variable is used for country specific data",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     * @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     * @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *  )
+     */
+
     //Get
     public function getDocuments() {
         $documents = Documents::all();
--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -8,12 +8,77 @@ use Illuminate\Http\Request;
 
 class RolesController extends Controller
 {
+     /**
+     * @OA\Get(
+     *      path="/roles",
+     *      operationId="getRolesList",
+     *      tags={"Roles"},
+
+     *      summary="Tous les roles",
+     *      description="Returns all countries and associated provinces. The country_slug variable is used for country specific data",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     * @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     * @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *  )
+     */
     public function getRolesList()
     {
         $roles = Roles::all();
         return response()->json($roles);
     }
+ /**
+     * @OA\Post(
+     *      path="/roles",
+     *      operationId="addRole",
+     *      tags={"Roles"},
 
+     *      summary="Tous les roles",
+     *      description="Returns all countries and associated provinces. The country_slug variable is used for country specific data",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     * @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     * @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *  )
+     */
     public function addRole(Request $request)
     {
         $role = new Roles();
