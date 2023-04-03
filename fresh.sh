b52267483febf410c142da96253be5bb2bdf758d
AezardSR <theplaygamer@gmail.com>
2023-04-03 16:28:53 +0200

    correction script.sh

--- fresh.sh
+++ fresh.sh
@@ -6,15 +6,19 @@ sudo docker compose down
 
 # Install php dependencies
 # FR: Installer les dépendances php
-sudo docker compose run --rm api composer install 
+sudo docker compose run --rm laravel composer install 
+
+# Launch containers
+# FR: Lancer les containers
+sudo docker compose up -d 
 
 # Fresh migrate + seeding
 # FR: Migrer et remplir la base de données
-sudo docker compose run --rm api php artisan migrate:fresh --seed 
+sudo docker compose run --rm laravel php artisan migrate:fresh --seed 
 
 # Generate key
 # FR: Generation de la clé
-sudo docker compose run --rm api php artisan key:generate 
+sudo docker compose run --rm laraval php artisan key:generate 
 
 # Launch containers
 # FR: Lancer les containers
